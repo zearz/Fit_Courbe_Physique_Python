e0b9ea1878c9822def971cbac12dbc17334df3db
zearz <56175142+zearz@users.noreply.github.com>
2022-02-01 21:07:33 +0100

    Update Analyse.py

--- Analyse.py
+++ Analyse.py
@@ -7,19 +7,19 @@ import numpy.polynomial.polynomial as poly
 def analyse(cheminDUfichier):
     data= np.loadtxt(cheminDUfichier, delimiter=';')
     x = data[:,1] #la deuxième colonne de donnée
-    t = data[:,0] #la première colonne de donnée
-    model1 = np.poly1d(np.polyfit(t, x, 1))
+    y = data[:,0] #la première colonne de donnée
+    model1 = np.poly1d(np.polyfit(y, x, 1))
     print(model1.c) #le fit en degrè 1
-    def f(t): #fonction pour tracer le fit 
-        return model1.c[0]*t+model1.c[1]
-    plt.plot(t, f(t), color='green',label = "Courbe fit (formule : {pre:f}x + {deu:f})".format(pre = model1.c[0],deu=model1.c[1])) #Plot le fit
-    plt.plot(t,x,label = "Courbe Sans fit") #plot la courbe
+    def f(y): #fonction pour tracer le fit 
+        return (model1.c[0]*y)+model1.c[1]
+    plt.plot(y, f(y), color='green',label = "Courbe fit (formule : {pre:f}x + {deu:f})".format(pre = model1.c[0],deu=model1.c[1])) #Plot le fit
+    plt.plot(y,x,label = "Courbe Sans fit") #plot la courbe
     plt.title(cheminDUfichier) #Nom du plot
 
 
 analyse("XXX") #Chemin d'accès pour le fichier
 
-plt.xlabel("Le temps en seconde") #Nom de l'axe x
-plt.ylabel("La température") #Nom de l'axe y
+plt.xlabel("L'axe X") #Nom de l'axe x
+plt.ylabel("L'axe Y") #Nom de l'axe y
 plt.legend()
 plt.show()
