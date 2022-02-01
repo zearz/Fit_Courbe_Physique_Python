e0f52b8672468a1988cd1aa9525284e255b9dce7
zearz <56175142+zearz@users.noreply.github.com>
2022-02-01 21:06:22 +0100

    Update Analyse.py

--- Analyse.py
+++ Analyse.py
@@ -2,8 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import numpy.polynomial.polynomial as poly
 
+#Pas de mot en premiere ligne des deux colonnes sinon ça marche pas et c'est dommage
+
 def analyse(cheminDUfichier):
-    data= np.loadtxt(chemin, delimiter=';')
+    data= np.loadtxt(cheminDUfichier, delimiter=';')
     x = data[:,1] #la deuxième colonne de donnée
     t = data[:,0] #la première colonne de donnée
     model1 = np.poly1d(np.polyfit(t, x, 1))
@@ -12,7 +14,7 @@ def analyse(cheminDUfichier):
         return model1.c[0]*t+model1.c[1]
     plt.plot(t, f(t), color='green',label = "Courbe fit (formule : {pre:f}x + {deu:f})".format(pre = model1.c[0],deu=model1.c[1])) #Plot le fit
     plt.plot(t,x,label = "Courbe Sans fit") #plot la courbe
-    plt.title(chemin) #Nom du plot
+    plt.title(cheminDUfichier) #Nom du plot
 
 
 analyse("XXX") #Chemin d'accès pour le fichier
